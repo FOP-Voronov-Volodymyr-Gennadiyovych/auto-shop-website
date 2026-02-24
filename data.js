f09d95759a6ca65d1e1a03db3f69c0587b4ba67b
Volodymyr <comprw@gmail.com>
2026-02-24 15:51:17 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+{
+  id: 1,
+  brand: "BMW",
+  model: "3 Series",
+  year: 2015,
+  price: 15000,
+  mileage: 85000,
+  fuel: "diesel",
+  color: "black",
+  images: ["image1.jpg", "image2.jpg"], // Add your photos
+  description: "Well maintained..."
+}

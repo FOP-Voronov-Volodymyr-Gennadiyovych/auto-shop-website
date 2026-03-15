7371bb85404be58509ebd1a53cf28d83f0fe53d2
Volodymyr <comprw@gmail.com>
2026-03-15 15:29:20 +0100

    Refactor car object properties to single line
    Condensed car object properties into a single line.

--- data.js
+++ data.js
@@ -1,11 +1,5 @@
 {
-  id: 1,
-  brand: "BMW",
-  model: "3 Series",
-  year: 2015,
-  price: 15000,
-  mileage: 85000,
-  fuel: "diesel",
+  id: 1, brand: "BMW", model: "3 Series", year: 2015, price: 15000, mileage: 85000, fuel: "diesel",
   color: "black",
   images: ["images/car1.jpg", "images/car1.jpg"], // Add your photos
   description: "Well maintained..."

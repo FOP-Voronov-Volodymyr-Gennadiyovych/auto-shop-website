f46c4aa34ef62933ac7c15a1b5534523f830be9f
Volodymyr <comprw@gmail.com>
2026-03-15 15:31:50 +0100

    Refactor car data structure in data.js

--- data.js
+++ data.js
@@ -1,6 +1,1 @@
-{
-  id: 1, brand: "BMW", model: "3 Series", year: 2015, price: 15000, mileage: 85000, fuel: "diesel",
-  color: "black",
-  images: ["images/car1.jpg", "images/car1.jpg"], // Add your photos
-  description: "Well maintained..."
-}
+{ id: 1, name: 'BMW', image: 'images/car1.jpg', price: 5500}

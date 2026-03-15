709414848b69a07101fb9467c3e551dbb3723a7c
Volodymyr <comprw@gmail.com>
2026-03-15 14:34:02 +0100

    Correct car data syntax for BMW entry
    Fixed syntax errors in car listings data.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 // Sample data for car listings
 const cars = [
-    { id: 1, name: 'Ford Mustang', image: 'mustang.jpg', price: 55000 },
+    { id: 1, name: 'BMW, image: 'foto_bmw.jpg', price: demo },
     { id: 2, name: 'Chevrolet Camaro', image: 'camaro.jpg', price: 45000 },
     { id: 3, name: 'Tesla Model S', image: 'tesla.jpg', price: 75000 },
 ];
